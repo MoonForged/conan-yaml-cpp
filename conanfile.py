c7dfede6b882d5b0aabaffeb7bf7b743b74666c1
Lee Berger <lee@moonforged.com>
2016-08-23 20:03:44 -0400

    Added Travis and AppVeyor support.

--- conanfile.py
+++ conanfile.py
@@ -24,17 +24,17 @@ class YamlCppConan( ConanFile ):
     shutil.move( '%s/CMakeLists.txt' % self.folder, '%s/CMakeListsOriginal.cmake' % self.folder )
 
     with open( '%s/CMakeLists.txt' % self.folder, 'w' ) as f:
-      print( textwrap.dedent( '''\
+      f.write( textwrap.dedent( '''\
         cmake_minimum_required( VERSION 2.8 )
         project( conanyamlcpp )
         include( ${CMAKE_CURRENT_SOURCE_DIR}/../conanbuildinfo.cmake )
         conan_basic_setup()
         include( "CMakeListsOriginal.cmake" )
-        ''' ), file=f )
+        ''' ) )
 
   def build( self ):
     cmake = CMake( self.settings )
-    flags = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+    flags = '-DBUILD_SHARED_LIBS=ON' if self.options.shared else ''
     self.run( 'cd %s && mkdir _build' % self.folder )
     configure_command = 'cd %s/_build && cmake .. %s' % ( self.folder, cmake.command_line )
     self.output.info( 'Configure with: %s' % configure_command )
@@ -44,9 +44,12 @@ class YamlCppConan( ConanFile ):
   def package( self ):
     self.copy( '*.h', dst='include', src='%s/include' % self.folder )
     self.copy( '*yaml-cpp*.lib', dst='lib', keep_path=False )
-    self.copy( '*.dll', dst='bin', keep_path=False )
-    self.copy( '*.so', dst='lib', keep_path=False )
-    self.copy( '*.a', dst='lib', keep_path=False )
+    self.copy( '*yaml-cpp*.dll', dst='bin', keep_path=False )
+    self.copy( '*yaml-cpp*.so', dst='lib', keep_path=False )
+    self.copy( '*yaml-cpp*.a', dst='lib', keep_path=False )
 
   def package_info( self ):
-    self.cpp_info.libs = [ 'libyaml-cppmd' ]
+    if self.settings.os == 'Windows':
+      self.cpp_info.libs = [ 'libyaml-cppmd' ]
+    else
+      self.cpp_info.libs = [ 'yaml-cpp' ]
